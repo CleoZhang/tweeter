33ff14792e4c1508ab7bf59d63f656e8977f48d9
CleoZhang <forest-zhang@hotmail.com>
2019-09-13 16:31:17 +0000

    Add counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,6 @@
-$('.new-tweet textarea').on('input', () => {
+$('.new-tweet textarea').on('input', function () {
   const len = $(this).val().length;
   const remaining = 140 - len;
+  console.log(len);
   $('.counter').text(remaining);
 });
